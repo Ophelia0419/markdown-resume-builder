85d884476b832a7ab5a9b38ab44c4c0680416cdc
Ophelia0419 <wnalstj016@naver.com>
2026-09-17 16:21:25 +0900

    feat: Spring Boot 초기 프로젝트 세팅

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "Aws_project"
---
+++ src/main/kotlin/com/example/aws_project/AwsProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.example.aws_project
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AwsProjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<AwsProjectApplication>(*args)
+}
